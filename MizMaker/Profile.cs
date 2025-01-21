b3880e5c62a4603b7f5ab3e44f9157c2db3e0851
s32191234 <s32191234@gmail.com>
2025-01-21 21:32:19 +0100

    Switch separator to comma ",".

--- MizMaker/Profile.cs
+++ MizMaker/Profile.cs
@@ -24,7 +24,7 @@ namespace MizMaker
 
         public static Profile FromString(string s)
         {
-            var cells = s.Split('\t');
+            var cells = s.Split(',');
 
             var profile = new Profile();
             profile.Name = cells[0];
